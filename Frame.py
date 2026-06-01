a999d12e322a4cdb066c870b972e1ad4b33de424
skinteh <skinteh@students.uni-mainz.de>
2026-06-01 19:33:11 +0200

    Documentation and functional adjustments

--- Frame.py
+++ Frame.py
@@ -42,11 +42,11 @@ class Frame(ndarray):
     CALLBACK_CALL_RETURN : Final = "call_return"
 
 
-    # TODO: soll class TrackBar composen
-    # TODO: solve the typing Frame|ndarray problem
     # TODO: mehrere Views and can immer neue Views erstellen mit der möglichkeit dies in sich zu speichern
     # TODO: each view should be refrenced with an index starting 0 - Main View
     # TODO: serializable functionality
+    # TODO: dragable point that determine px distance to other defined points irt and a variant where the distances are shown on the connecting lines using self.nearest
+    # TODO: toFloat, toUint8, toFrequencyDomain, toSpatialDomain (Frame should have __<VARIABLE> with name DOMAIN_TYPE)
     def __new__(cls, src : str|ndarray, window_name : str, slider : Slider = None, updatable : bool = True) -> "Frame|ndarray":
 
         # read in data and cast to subclass
@@ -84,17 +84,41 @@ class Frame(ndarray):
         if waitKey is not None: return self.waitUserKey(waitKey)
         return None
 
-    def destroy(self):
+    def destroy(self) -> None:
+        """
+        destroys all active windows
+        :return: None
+        """
+
         cv.destroyWindow(self.window_name)
 
     def setWindowName(self, window_name : str) -> "Frame":
-        cv.namedWindow(window_name)     # TODO wird neues window erstellt wenn gecallt (nicht erwünscht)
+        """
+        Gives frame a new window name and calls cv.namedWindow on it
+        :param window_name: new window name for frame
+        :return: returns frame instance with new window name
+        """
+
+        cv.namedWindow(window_name)
         self.window_name = window_name
         return self
 
-    def toGray(self) -> ndarray: return self(cv.cvtColor, code=cv.COLOR_BGR2GRAY)
+    def toGray(self) -> "Frame":
+        """
+        :return: gray scale version of frame
+        """
+
+        return self(cv.cvtColor, code=cv.COLOR_BGR2GRAY)
 
     def rescale(self, scale : float = 1, isIncremental : bool = False) -> "Frame|ndarray":
+
+        """
+        This function scales a frame to a desired resolution with a scaler where 1 means no scaling and .5 means half the original size
+
+        :param scale: scaler to scale with
+        :param isIncremental: sets if the current scale should be replaced or just decremented (negative scale to increment)
+        :return: returns a scaled frame
+        """
 
         if self.updatable and (not scale == 1 and not isIncremental):
             self.scale = scale if not isIncremental else self.scale - scale
@@ -107,24 +131,38 @@ class Frame(ndarray):
 
     def draw_point(self, point, radius : int = 5, color: tuple[int, int, int] = (0, 0, 255), thickness: int = 3):
 
+        """
+        This function draws a circle around point(s) on frame
+
+        :param point: sets of (x, y) coordinates that should be drawn on frame
+        :param radius: radius of circle
+        :param color: color of the circle(s)
+        :param thickness: thickness of the circumference
+        :return: returns back frame with the points
+        """
+
+        if not point: return self
         def __draw(points): self(cv.circle, center=points, radius=radius, color=color, thickness=thickness)
 
-        np.vectorize(__draw, signature="(2) -> ()")(point)
+        np.vectorize(__draw, signature="(2) -> ()")(point)      # todo draw_point() = __vectorized_draw_point()
         return self
 
 
     def set_mouse_callback(self, **kwargs) -> None:
         """
-        functionalities:
-        - double left click to add a point
-        - double right click to remove a point in the area
+        Callback function for on-click events on Frame, based on OpenCV cv.setMouseCallback.
+        \nFunctionalities:
+        \n- double (left) click to add a point
+        \n- double (left) click + shift to remove a point in the area
 
-        :param kwargs: must have kwargs: amount, points, type
+        :param kwargs: must have kwargs: amount : list, points : list, TODO type
         :return: None
         """
+
         # TODO implement type with CALLBACK_TYPE_<POINTS, AREA>
         # TODO implement min_distance allowed between points as optional parameter
-        assert kwargs.get(self.CALLBACK_AMOUNT) is not None and isinstance(kwargs.get(self.CALLBACK_AMOUNT), int), "Mouse callback must have kwarg: amount"
+        # TODO allow user to set custom callback function but through a standard pipeline
+        assert kwargs.get(self.CALLBACK_AMOUNT) is not None and isinstance(kwargs.get(self.CALLBACK_AMOUNT), list), "Mouse callback must have kwarg: amount"
         assert kwargs.get(self.CALLBACK_POINTS) is not None and isinstance(kwargs.get(self.CALLBACK_POINTS), list), "Mouse callback must have kwarg: points"
         cv.setMouseCallback(self.window_name, self.__select_points, param=kwargs)
 
@@ -133,11 +171,11 @@ class Frame(ndarray):
         points : list = params[Frame.CALLBACK_POINTS]
         if event == cv.EVENT_LBUTTONDBLCLK and flag == 1:             # only double left -> add
 
-            amount = params[Frame.CALLBACK_AMOUNT]
+            amount = params[Frame.CALLBACK_AMOUNT][0]
             if len(points) < amount:
                 if len(points) > 0:
                     _, distances = self.nearest_point((x, y), points)
-                    # todo if point in array or in close prorimity dont add, for now 20px
+                    # todo if point in array or in close prorimity dont add, for now 20px, later additional optional argument
                     if distances[0] < 20:
                         print("[INFO] point already selected")
                     else:
@@ -151,7 +189,7 @@ class Frame(ndarray):
             if len(points) > 0:
                 i, distances = self.nearest_point((x, y), points)
                 if distances[0] < 20:
-                    print(points.pop(i))
+                    points.pop(i)
                     print(f"[INFO] current points after removal: {len(points)}")
                 else: print(f"[INFO] no points in range")
 
@@ -167,21 +205,29 @@ class Frame(ndarray):
     def __select_area(self, event : int, x : int, y : int, flag : int, params : dict): ...
 
     @staticmethod
-    def nearest_point(point : tuple, points : list[tuple[int, int]]) -> tuple:
+    def nearest_point(point : tuple, points : list[tuple[int, int]], flag : int = 0) -> tuple|None:
 
         """
-
+        Checks the distance from a point to all other points
         :param point: point to check distance of
         :param points: all the points to be tested against
-        :return: returns index of the point with the min distance, and as sorted list (ascending) of the distance values
+        :param flag: 0 for value sorting and 1 for index sorting, else returns None
+        :return: returns index of the point with the min distance, and a sorted list (ascending) of the distance values
         """
         #TODO vectorize func
 
         px_distances = np.linalg.norm(np.array(point) - np.array(points), axis=1)   # broadcasts point to N x 2 and substracts from N x 2
-        return np.argmin(px_distances), np.sort(px_distances)
+        if flag == 0: return np.argmin(px_distances), np.sort(px_distances)
+        elif flag == 1: return np.argmin(px_distances), np.argsort(px_distances)
+        return None
 
     @staticmethod
     def waitUserKey(ms : int) -> int:
+        """
+        a key on-click listener with timeout-timer in milliseconds
+        :param ms: timer in ms
+        :return: returns key pressed, if nothing was registered, returns 255
+        """
         assert isinstance(ms, int), "WaitKey timeout must always be an integer in milliseconds"
         key = cv.waitKey(ms) & 0xFF
         return key
@@ -207,9 +253,15 @@ class Frame(ndarray):
     def __iter__(self) -> "Frame":
         return self
 
-    def __call__(self, func : Callable[[ndarray, ...], ndarray], **kwargs) -> "Frame|ndarray":
+    def __call__(self, func : Callable[["Frame|ndarray", ...], "Frame|ndarray"], **kwargs) -> "Frame":
+        """
+        call frame to run a function (transformation) on the frame
+        :param func: a python function that gives back a frame
+        :param kwargs: the keyword arguments of the function
+        :return:
+        """
 
-        assert isinstance(func, Callable), "Passed argument is not a function"
+        assert isinstance(func, Callable), "Passed argument to call is not a function"
         if self.updatable:
             try:
                 call = func(self, dst=self, **kwargs)
@@ -220,9 +272,9 @@ class Frame(ndarray):
                 call = func(self, **kwargs)
 
             assert isinstance(call, ndarray), f"Function {func.__name__} didn't return the expected type"
-        return self._refresh_frame(call)
+        return self.__refresh_frame(call)
 
-    def _refresh_frame(self, src : str | ndarray) -> "Frame|ndarray|None":
+    def __refresh_frame(self, src : str | ndarray) -> "Frame":
 
         # if hasDst: return None
         obj = self.__new__(
@@ -258,7 +310,7 @@ if __name__ == "__main__":
     # print(frameObj[:, :, :].setWindowName("Sambou Kinteh")(cv.cvtColor, code=cv.COLOR_BGR2RGB).toGray()[:, :].rescale(2).show(10000))
 
     data = {
-        Frame.CALLBACK_AMOUNT: 3,
+        Frame.CALLBACK_AMOUNT: [3],
         Frame.CALLBACK_POINTS: []
     }
     frameObj.set_mouse_callback(**data)
--- TrackBar.py
+++ TrackBar.py
@@ -7,13 +7,24 @@ class Slider:
 
     def __init__(self, window_name : str, params : dict[str, list]):
 
+        """
+        A trackbar class based on the opencv cv.createTrackbar function
+        :param window_name: Name of the window for the Trackbar, can be same as frame or different
+        :param params: Keyword dict of all parameters of the trackbar. Every keys should be same as the keyword arguments for thier corresponding functions. Each key takes a list as value that has two values, start value and number of possible values starting from 0
+        """
+
         self.window_name = window_name
         self.__original_params = params
         self.scale = []
         self.params : list[str] = []
         self.last_params : dict = {}
 
-    def __call__(self):
+    def __call__(self) -> None:
+
+        """
+        initiates (starts) the slider
+        :return: None
+        """
 
         cv.namedWindow(self.window_name)
         for slider_key in self.__original_params:
@@ -21,9 +32,19 @@ class Slider:
             self.add((slider_key, slider_range))
 
     def param(self, param) -> int:
+
+        """
+        :param param: parameter name
+        :return: current value of a single parameter
+        """
+        assert param in self.params, "Invalid parameter for slider"
         return cv.getTrackbarPos(param, self.window_name)
 
     def get_params(self) -> dict[str, int]:
+
+        """
+        :return: returns list of all current parameter value pairs
+        """
         return dict([(param, self.param(param)) for param in self.params])
 
     # @params.setter
@@ -32,6 +53,12 @@ class Slider:
     #     self.params = value
 
     def add(self, param : tuple[str, list]) -> None:
+
+        """
+        adds a single parameter to the slider instance. new parameters can be added at any time
+        :param param: parameter name and range tuple, see class definition for more explanation
+        :return: None
+        """
         assert isinstance(param, tuple), "Invalid parameter data"
         self.params.append(param[0])
         cv.createTrackbar(param[0], self.window_name, param[1][0], param[1][1], lambda _ : _)
